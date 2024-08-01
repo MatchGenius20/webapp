8966eaf9c643efddfea0932c6be2d497b7133bf0
Amrit <amritsinghal2211@gmail.com>
2024-08-01 23:23:07 +0530

    added ui related changes

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -42,7 +42,7 @@ const Signup: React.FC<SignupProps> = ({ onClose }) => {
   return (
     <div className="bg-white rounded-lg  max-w-sm w-5xl">
       <div className="flex justify-between items-center mb-6">
-        <h2 className="text-2xl font-bold text-[#443EDE]">Signup</h2>
+        <h2 className="text-2xl font-bold text-[#443EDE]">Register</h2>
         <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
           <svg
             className="h-6 w-6"

a7758f15f15c8428d667d9cbb0e5226d84bb7006
Amrit <amritsinghal2211@gmail.com>
2024-08-04 23:07:46 +0530

    completed profile settings and payment page

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,18 @@
-import Link from 'next/link'
-import React from 'react'
-import { SidebarProps } from '../../type'
+import Link from 'next/link';
+import React from 'react';
+import { SidebarProps } from '../../type';
 
 const Sidebar: React.FC<SidebarProps> = ({ selected, onSelect }) => {
   return (
-    <div className="w-64 bg-[#FFFFFF] border-r-2 border-[#B2AFF8] h-screen p-4">
-      <h1 className="text-xl font-bold mb-8 text-[#443EDE]">
-        <Link href={'/'}>DebatesMatch</Link>
-      </h1>
-      <ul>
+        <div className="relative w-64 bg-[#FFFFFF] border-r-2 border-[#B2AFF8] h-screen p-4">
+    <div className="absolute top-0 left-0 right-0 bg-[#EDECFF] p-4">
+        <h1 className="text-xl font-bold text-[#443EDE]">
+          <Link href={'/'}>DebatesMatch</Link>
+        </h1>
+      </div>
+      <ul className="pt-16"> {/* Adjust padding to ensure the list items are not hidden behind the header */}
         <li
-          className={`p-4  cursor-pointer mb-2 ${selected === 'ProfileSettings' ? 'bg-[#453EF1] text-white rounded-lg' : 'text-black'}`}
+          className={`p-4 cursor-pointer mb-2 ${selected === 'ProfileSettings' ? 'bg-[#453EF1] text-white rounded-lg' : 'text-black'}`}
           onClick={() => onSelect('ProfileSettings')}
         >
           Profile Settings
@@ -29,7 +31,7 @@ const Sidebar: React.FC<SidebarProps> = ({ selected, onSelect }) => {
         </li>
       </ul>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
